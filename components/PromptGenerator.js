89fb798c017b4f6e32deb39b021b0526ac0ff92c
Muhammad Miftahus Surur <97358791+muhammadmiftahs@users.noreply.github.com>
2026-09-15 23:47:31 +0700

    Fixedd Bug

--- components/PromptGenerator.js
+++ components/PromptGenerator.js
@@ -1252,7 +1252,7 @@ export default function PromptGenerator() {
               );
             })}
           </div>
-        </div>
+        </>
       )}
 
       <HistoryPanel
--- components/VideoPromptGenerator.js
+++ components/VideoPromptGenerator.js
@@ -1152,6 +1152,7 @@ export default function VideoPromptGenerator() {
             );
           })}
         </div>
+        </>
       )}
 
       <HistoryPanel
